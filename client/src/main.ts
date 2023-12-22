f2c87cb4cbf8ae6f3f6636ebe7b5b3991c1f5d47
flynncao <flynncao@yahoo.com>
2023-12-22 19:04:15 +0800

    feat: daisyui, layout updated

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,9 +6,6 @@ import App from './App.vue'
 
 //  CSS
 import './styles/main.css'
-import 'primevue/resources/themes/lara-light-indigo/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 const router = createRouter({
---
+++ client/src/pages/menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="col-span-2 col-start-2 min-h-[20px] bg-blue-400">
+    1
+  </div>
+  <div class="min-h-[20px] bg-red-400">
+    <!-- <button class="btn-secondary grid-span-3 btn" :disabled="!name">
+              Scrape Definitions Instead
+            </button> -->
+    2
+  </div>
+</template>
